9a50c402a085330cc365838864e6bde08a345572
mkd <monkeydioude@gmail.com>
2024-09-04 16:08:39 +0200

    handle more content type rss

--- src/services/channels.rs
+++ src/services/channels.rs
@@ -6,13 +6,18 @@ pub fn compute_refresh_avg(current_avg: f32, time_to_refresh: i64, refresh_count
         / ((refresh_count + 1) as i64 * 1000)) as f32
 }
 
+static RSS_CONTENT_TYPES: [&str; 2] = ["application/xml", "application/rss"];
+
 pub async fn find_out_source_type(channel_name: &str) -> Result<SourceType, Error> {
     let response = reqwest::get(channel_name).await?;
     let content_type = match response.headers().get("content-type") {
         Some(header) => header.to_str().unwrap_or_default(),
         None => "",
     };
-    if content_type.find("application/xml").is_some() {
+    if RSS_CONTENT_TYPES
+        .iter()
+        .any(|ct| content_type.find(ct).is_some())
+    {
         return Ok(SourceType::RSSFeed);
     }
     Ok(SourceType::Bakery)
